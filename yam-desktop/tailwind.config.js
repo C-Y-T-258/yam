d255ef98a31ada1c9d2d0ab68d2f0a6f945d32c1
12721 <1272100921@qq.com>
2026-07-08 21:50:16 +0800

    feat: scaffold Tauri + React project

---
+++ yam-desktop/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0F3460',
+        accent: '#E94560',
+      },
+    },
+  },
+  plugins: [],
+}
